5fecededc2fc58f11de153ad599d587f543058da
Bharadwaj MP <bharadwaj.mp.97@gmail.com>
2018-03-27 18:42:06 +0530

    .

---
+++ BharadwajMP/cohenSutherland.c
@@ -0,0 +1,171 @@
+#include<stdio.h>
+#include<GL/glut.h>
+
+double xmin = 50,ymin = 50,xmax = 100,ymax = 100;
+double xvmin = 200,yvmin = 200,xvmax = 300,yvmax = 300;
+float *px0,*py0,*px1,*py1;
+const int R = 2;
+const int L = 1;
+const int T = 8;
+const int B = 4;
+int n;
+int computeOutcode(double x,double y);
+
+void cohenSutherland(double x0,double y0,double x1,double y1)
+{
+	int oc0,oc1,oc;
+	int accept = 0,done = 0;
+	oc0 = computeOutcode(x0,y0);
+	oc1 = computeOutcode(x1,y1);
+	//printf("oc0 = %d oc1 = %d\n",oc0,oc1);
+	do
+	{
+		
+		if(!(oc0 | oc1))
+		{
+			accept = 1;
+			done = 1;
+		}
+		else if (oc0 & oc1)
+		{
+			done = 1;
+		}
+		else
+		{
+			double x,y;
+
+			oc = oc0? oc0 : oc1;
+
+			if(oc & T)
+			{
+				x = x0 + (x1 - x0)*(ymax - y0)/(y1 - y0);
+				y = ymax;
+			}
+			else if(oc & B)
+			{
+				x = x0 + (x1 - x0)*(ymin - y0)/(y1 - y0);
+				y = ymin;
+			}
+			else if(oc & R)
+			{
+				y = y0 + (y1 - y0)*(xmax - x0)/(x1 - x0);
+				x = xmax;
+			}
+			else
+			{
+				y = y0 + (y1 - y0)*(xmin - x0)/(x1 - x0);
+				x = xmin;
+			}
+
+			if(oc == oc0)
+			{
+				x0 = x;
+				y0 = y;
+				oc0 = computeOutcode(x0,y0);
+			}
+			else
+			{
+				x1 = x;
+				y1 = y;
+				oc1 = computeOutcode(x1,y1);
+			}
+		}
+	}while(!done);
+
+	if(accept)
+	{
+		double sx = (xvmax - xvmin)/(xmax - xmin);
+		double sy = (yvmax - yvmin)/(ymax - ymin);
+		double vx0 = xvmin + (x0 - xmin)*sx;
+		double vx1 = xvmin + (x1 - xmin)*sx;
+		double vy0 = yvmin + (y0 - ymin)*sy;
+		double vy1 = yvmin + (y1 - ymin)*sy;
+
+		glColor3f(0,0,0);
+		glBegin(GL_LINE_LOOP);
+			glVertex2f(xvmin,yvmin);
+			glVertex2f(xvmax,yvmin);
+			glVertex2f(xvmax,yvmax);
+			glVertex2f(xvmin,yvmax);
+		glEnd();
+
+		glColor3f(0,1,0);
+		glBegin(GL_LINES);
+			glVertex2d(vx0,vy0);
+			glVertex2d(vx1,vy1);
+		glEnd();
+
+//printf("%f %f , %f %f\n",vx0,vy0,vx1,vy1);
+	}
+}
+
+int computeOutcode(double x,double y)
+{
+	int code = 0;
+
+	if(y > ymax)
+		code |= T;
+	if(y < ymin)
+		code |= B;
+	if(x > xmax)
+		code |= R;
+	if(x < xmin)
+		code |= L;
+	return code;
+}
+
+void display()
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	glColor3f(0,0,1);
+	glBegin(GL_LINE_LOOP);
+		glVertex2f(xmin,ymin);
+		glVertex2f(xmax,ymin);
+		glVertex2f(xmax,ymax);
+		glVertex2f(xmin,ymax);
+	glEnd();
+	
+	int i = 0;
+	for(i = 0;i < n;i++)
+	{
+		glBegin(GL_LINES);
+			glVertex2d(px0[i],py0[i]);
+			glVertex2d(px1[i],py1[i]);
+		glEnd();
+		cohenSutherland(px0[i],py0[i],px1[i],py1[i]);
+	//	printf("Got input\n");
+		glColor3f(1,0,0);
+	} 
+	glFlush();
+}
+
+void myinit()
+{
+	glClearColor(1,1,1,1);
+	glColor3f(1,0,0);
+	gluOrtho2D(0,500,0,500);
+}
+
+void main(int argc,char **argv)
+{
+	printf("Enter number of lines :");
+	scanf("%d",&n);
+	px0 = (float *)malloc(sizeof(float));
+	py0 = (float *)malloc(sizeof(float));
+	px1 = (float *)malloc(sizeof(float));
+	py1 = (float *)malloc(sizeof(float));
+	int i = 0;
+	for(i = 0;i < n;i++)
+	{
+		printf("\nEnter two points for line %d :",(i+1));
+		scanf("%f%f%f%f",&px0[i],&py0[i],&px1[i],&py1[i]);
+	}
+	glutInit(&argc,argv);
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+	glutInitWindowSize(500,500);
+	glutCreateWindow("Clipping");
+	glutDisplayFunc(display);
+	myinit();
+	glutMainLoop();
+}
---
+++ BharadwajMP/scanLine.c
@@ -0,0 +1,133 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<GL/glut.h>
+
+int x1 = 200,y1 = 200,x2 = 100,y2 = 300,x3 = 200,y3 = 400,x4 = 300,y4 = 300;
+void edgeDetect(float x1,float y1,float x2,float y2,int *le,int *re)
+{
+	float mx,x,temp;
+	int i;
+
+	if((y2 - y1) < 0)
+	{
+		temp = y1;
+		y1 = y2;
+		y2 = temp;
+
+		temp = x1;
+		x1 = x2;
+		x2 = temp;
+	}
+
+	if((y2 - y1) != 0)
+		mx = (x2 - x1)/(y2 - y1);
+	else
+		mx = x2 - x1;
+	x = x1;
+
+	for(i = y1;i <= y2;i++)
+	{
+		if(x < le[i])
+			le[i] = (int)x;
+		if(x > re[i])
+			re[i] = (int)x;
+		x += mx;
+	}
+}
+
+void draw_pixel(int x,int y)
+{
+	glColor3f(0,1,0);
+	glBegin(GL_POINTS);
+		glVertex2i(x,y);
+	glEnd();
+	glFlush();
+} 
+
+void scanfill(float x1,float y1,float x2,float y2,float x3,float y3,float x4,float y4)
+{
+	int le[500],re[500];
+	int i,y;
+
+	for(i = 0;i < 500;i++)
+	{
+		le[i] = 500;
+		re[i] = 0;
+	}
+
+	edgeDetect(x1,y1,x2,y2,le,re);
+	edgeDetect(x2,y2,x3,y3,le,re);
+	edgeDetect(x3,y3,x4,y4,le,re);
+	edgeDetect(x4,y4,x1,y1,le,re);
+
+	for(y = 0;y < 500;y++)
+	{
+		if(le[y] <= re[y])
+			for(i = (int)le[y];i <= (int)re[y];i++)
+				draw_pixel(i,y);
+	}
+}
+
+void display()
+{
+//	int x1 = 200,y1 = 200,x2 = 100,y2 = 300,x3 = 200,y3 = 400,x4 = 300,y4 = 300;
+//	int x1 = 200,y1 = 200,x2 = 100,y2 = 200,x3 = 100,y3 = 300,x4 = 200,y4 = 300;
+//	printf("Enter polygon points:");
+//	scanf("%d%d%d%d%d%d%d");
+	
+
+//	scanfill(x1,y1,x2,y2,x3,y3,x4,y4);
+
+	glFlush();
+}
+
+void myinit()
+{
+	glClearColor(1,1,1,1);
+	// glColor3f(1,0,0);
+	glPointSize(1);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluOrtho2D(0,500,0,500);
+	glClear(GL_COLOR_BUFFER_BIT);
+	glColor3f(0,1,0);
+}
+
+void myMenu(int id)
+{
+	if(id == 1)
+	{
+		glClear(GL_COLOR_BUFFER_BIT);
+	}
+	if(id == 3)
+		exit(-1);
+	if(id == 2)
+	{
+		glBegin(GL_LINE_LOOP);
+			glVertex2f(x1,y1);
+			glVertex2f(x2,y2);
+			glVertex2f(x3,y3);
+			glVertex2f(x4,y4);
+		glEnd();
+		glFlush();
+		scanfill(x1,y1,x2,y2,x3,y3,x4,y4);
+		glFlush();
+	}
+}
+
+void main(int argc,char **argv)
+{
+	glutInit(&argc,argv);
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+	glutInitWindowSize(500,500);
+	glutInitWindowPosition(0,0);
+	glutCreateWindow("Scanline filling");
+	//glutDisplayFunc(display);
+	int menu_id = glutCreateMenu(myMenu);
+	glutAddMenuEntry("Clear screen",1);
+	glutAddMenuEntry("Fill polygon",2);
+	glutAddMenuEntry("Exit",3);
+	glutAttachMenu(GLUT_RIGHT_BUTTON);
+	myinit();
+	glutMainLoop();
+}
